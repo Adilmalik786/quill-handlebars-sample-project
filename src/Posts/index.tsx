7ce4f2b6106fccd254865afad12891a6a604dec9
Adilmalik786 <adilmalik.infinity@gmail.com>
2021-01-24 01:03:40 +0500

    Dropdown styling issue fixed.

--- src/Posts/index.tsx
+++ src/Posts/index.tsx
@@ -9,6 +9,8 @@ import {
 } from "react-admin";
 import RichTextInput from "ra-input-rich-text";
 
+import { makeStyles } from '@material-ui/core/styles'
+
 import "quill-handlebars/dist/quill.handlebars.css";
 
 import "quill-handlebars";
@@ -24,52 +26,65 @@ export const PostList = (props) => (
 const atValues = [
   { id: 1, value: "Fredrik Sundqvist" },
   { id: 2, value: "Patrik Sjölin" },
+  { id: 1, value: "Adil Malik" },
+  { id: 2, value: "Hunter" },
+  { id: 1, value: "Conner" },
+  { id: 2, value: "Elizabeth" },
+
 ];
 const hashValues = [
   { id: 3, value: "Fredrik Sundqvist 2" },
   { id: 4, value: "Patrik Sjölin 2" },
 ];
 
-export const PostCreate = (props) => (
-  <Fragment>
-    <Create {...props}>
-      <SimpleForm>
-        <TextInput source="title" />
-        <RichTextInput
-          source="body"
-          options={{
-            modules:{
-            handlebars: {
-              allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
-              handlebarsDenotationChars: ["{{"],
-              source: function (searchTerm, renderList, handlebarsChar) {
-                let values;
+export const PostCreate = (props) => {
+  const useStyles = makeStyles({
+    createPost:{ '& div': {overflow: 'visible !important' }}
+});
 
-                if (handlebarsChar === "{{") {
-                  values = atValues;
-                } else {
-                  values = hashValues;
-                }
+const classes = useStyles();
 
-                if (searchTerm.length === 0) {
-                  renderList(values, searchTerm);
-                } else {
-                  const matches = [];
-                  for (let i = 0; i < values.length; i++)
-                    if (
-                      ~values[i].value
-                        .toLowerCase()
-                        .indexOf(searchTerm.toLowerCase())
-                    )
-                      matches.push(values[i]);
-                  renderList(matches, searchTerm);
-                }
+  return (
+    <Fragment>
+      <Create className={classes.createPost} {...props}>
+        <SimpleForm >
+          <TextInput source="title" />
+          <RichTextInput
+            source="body"
+            options={{
+              modules:{
+              handlebars: {
+                allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+                handlebarsDenotationChars: ["{{"],
+                source: function (searchTerm, renderList, handlebarsChar) {
+                  let values;
+  
+                  if (handlebarsChar === "{{") {
+                    values = atValues;
+                  } else {
+                    values = hashValues;
+                  }
+  
+                  if (searchTerm.length === 0) {
+                    renderList(values, searchTerm);
+                  } else {
+                    const matches = [];
+                    for (let i = 0; i < values.length; i++)
+                      if (
+                        ~values[i].value
+                          .toLowerCase()
+                          .indexOf(searchTerm.toLowerCase())
+                      )
+                        matches.push(values[i]);
+                    renderList(matches, searchTerm);
+                  }
+                },
               },
-            },
-          }
-          }}
-        />
-      </SimpleForm>
-    </Create>
-  </Fragment>
-);
+            }
+            }}
+          />
+        </SimpleForm>
+      </Create>
+    </Fragment>
+  );
+}
